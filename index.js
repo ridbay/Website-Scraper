4f93d97f9737a24ef4e2be17cc93cd8eb7ae7e79
ridbay <balogunridwan@gmail.com>
2023-08-02 12:52:54 +0100

    Made few changes

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
-const scrape = require('website-scraper');
+const scrape = require("website-scraper");
 
+const base_url = "https://www.nuprc.gov.ng";
 scrape({
-    urls: [
-        'http://balogunridwa.com/index.html', // Will be saved with default filename 'index.html'
-        {
-            url: 'http://balogunridwa.com/about.html',
-            filename: 'about.html'
-        },
-        {
-            url: 'http://balogunridwa.com/services.html',
-            filename: 'services.html'
-        },
-        {
-            url: 'http://balogunridwa.com/services-details.html',
-            filename: 'farm.html'
-        },
-        {
-            url: 'http://balogunridwa.com/contact.html',
-            filename: 'contact.html'
-        }
-    ],
-    directory: './scraped-website',
-    subdirectories: [
-        {
-            directory: 'img',
-            extensions: ['.jpg', '.png', '.svg']
-        },
-        {
-            directory: 'js',
-            extensions: ['.js']
-        },
-        {
-            directory: 'css',
-            extensions: ['.css']
-        },
-        {
-            directory: 'fonts',
-            extensions: ['.woff','.ttf']
-        }
-    ],
-    sources: [
-        {
-            selector: 'img',
-            attr: 'src'
-        },
-        {
-            selector: 'link[rel="stylesheet"]',
-            attr: 'href'
-        },
-        {
-            selector: 'script',
-            attr: 'src'
-        }
-    ]
-}).then(function (result) {
-    // Outputs HTML 
+  urls: [
+    `${base_url}/index.php`, // Will be saved with default filename 'index.html'
+    {
+      url: `${base_url}/history-of-dpr/`,
+      filename: "history-of-dpr.html",
+    },
+    {
+      url: `${base_url}/value-statement/`,
+      filename: "value-statement.html",
+    },
+  ],
+  urls: ["https://www.nuprc.gov.ng/index.php"],
+  directory: "./scraped-website",
+  subdirectories: [
+    {
+      directory: "img",
+      extensions: [".jpg", ".png", ".svg"],
+    },
+    {
+      directory: "js",
+      extensions: [".js"],
+    },
+    {
+      directory: "css",
+      extensions: [".css"],
+    },
+    {
+      directory: "fonts",
+      extensions: [".woff", ".ttf"],
+    },
+  ],
+  sources: [
+    {
+      selector: "img",
+      attr: "src",
+    },
+    {
+      selector: 'link[rel="stylesheet"]',
+      attr: "href",
+    },
+    {
+      selector: "script",
+      attr: "src",
+    },
+  ],
+})
+  .then(function (result) {
+    // Outputs HTML
     // console.log(result);
     console.log("Content succesfully downloaded");
-}).catch(function (err) {
+  })
+  .catch(function (err) {
     console.log(err);
-});
+  });
